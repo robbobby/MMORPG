0d34c62b96b7405c162a54c8010082a4807077e6
robbobby <69080884+robbobby@users.noreply.github.com>
2020-12-24 13:48:04 +0000

    Movement through server set and fixed

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -5,57 +5,59 @@ using static UnityEngine.Camera;
 
 namespace Player {
     public class Movement : NetworkBehaviour {
-        [SerializeField] Transform target;
         private Camera m_camera;
+        private Animator m_animator;
+        private NavMeshAgent m_navMeshAgent;
 
         private void Start() {
+            m_animator = GetComponent<Animator>();
+            m_navMeshAgent = GetComponent<NavMeshAgent>();
             m_camera = Camera.main;
         }
-
-        public override void OnStartLocalPlayer() {
-            if (!main) return;
-        }
         private void Update() {
-            UpdateCameraPosition();
-
-            if(!isLocalPlayer) { // Exit if not client does not own object
-                return;
-            }
-            if (Input.GetMouseButton(0)) {
-                MoveToCursor();
-            }
-            if (Input.GetMouseButtonUp(0)) {
-                StopMovement();
-            }
+            if(!isLocalPlayer) return; // Exit if not client does not own object
             UpdateAnimator();
+            UpdateCameraPosition();
+            HandleMouseClick();
         }
-
-        private void UpdateCameraPosition() {
-            Vector3 positionTranslate = transform.localPosition;
-            if (!(m_camera is null)) {
-                m_camera.transform.localPosition = new Vector3(positionTranslate.x,
-                    positionTranslate.y + 5f,
-                    positionTranslate.z - 8f);
-                m_camera.transform.localEulerAngles = new Vector3(30f, 0f, 0f);
+        [Command] private void CmdValidateMouseButtonDown(Ray ray, Vector3 hitInfoPoint) {
+            RpcMoveToCursor(hitInfoPoint);
+        }
+        [Command] private void CmdValidateMouseButtonUp() {
+            RpcStopMovement();
+        }
+        [Client] private void HandleMouseClick() {
+            if(!isLocalPlayer) return;
+            if (Input.GetMouseButton(0)) {
+                Ray ray = m_camera.ScreenPointToRay(Input.mousePosition);
+                bool hasHit = Physics.Raycast(ray, out RaycastHit positionToMoveTo);
+                if (hasHit) {
+                    CmdValidateMouseButtonDown(ray, positionToMoveTo.point);
+                }
             }
+            if (Input.GetMouseButtonUp(0))   CmdValidateMouseButtonUp();
         }
-
-        private void StopMovement() { // Needs work, stops too abruptly
-            GetComponent<NavMeshAgent>().destination = GetComponent<NavMeshAgent>().nextPosition;
+        [Client] private void UpdateCameraPosition() {
+            Vector3 positionTranslate = transform.localPosition;
+            if (m_camera is null) return;
+            m_camera.transform.localPosition = new Vector3(positionTranslate.x,
+                positionTranslate.y + 5f,
+                positionTranslate.z - 8f);
+            m_camera.transform.localEulerAngles = new Vector3(30f, 0f, 0f);
         }
-
-        private void UpdateAnimator() {
+        [Client] private void UpdateAnimator() {
             Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);
             float speed = localVelocity.z;
-            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+            m_animator.SetFloat("forwardSpeed", speed);
         }
-        private void MoveToCursor() {
-            Ray ray = m_camera.ScreenPointToRay(Input.mousePosition);
-            bool hasHit = Physics.Raycast(ray, out var positionToMoveTo);
-            if (hasHit) {
-                GetComponent<NavMeshAgent>().destination = positionToMoveTo.point;
+        [ClientRpc] private void RpcStopMovement() { // Needs work, stops too abruptly
+            GetComponent<NavMeshAgent>().destination = m_navMeshAgent.nextPosition;
+        }
+        [ClientRpc]
+        private void RpcMoveToCursor(Vector3 destination) {
+            m_navMeshAgent.SetDestination(destination);
+            // UpdateAnimator();
             }
         }
     }
-}
